ea6a3ef9738c13493d0ae1a5a933d50a220c7f83
Aaron <aaron950415@qq.com>
2020-09-03 03:22:15 +1000

    NumberPad部分js完成

--- src/components/money/NumberPad.vue
+++ src/components/money/NumberPad.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="numberPad noselect">
-    <div class="output">0</div>
-    <div class="buttons">
+    <div class="output">{{output}}</div>
+    <div @click="choosed" class="buttons">
       <button>1</button>
       <button>2</button>
       <button>2</button>
@@ -20,58 +20,109 @@
   </div>
 </template>
 
-<script>
-export default{
-  name:'NumberPad'
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+@Component
+export default class NumberPad extends Vue {
+  output = "0";
+
+  choosed(e: MouseEvent) {
+    if ((e.target as HTMLElement).className === "buttons") {
+      return;
+    }
+    const input = (e.target as HTMLElement).innerHTML;
+    if (this.output === "0") {
+      if ("0123456789".indexOf(input) >= 0) {
+        this.output = input;
+      } else if (input === ".") {
+        this.output += input;
+      }
+      return;
+    }
+    if (input === "." && this.output.indexOf(".") >= 0) {
+      return;
+    } else {
+      if (input === "Del") {
+        this.remove();
+        return;
+      } else if (input === "Clear") {
+        this.clear();
+        return;
+      }
+      if (input === "OK") {
+        this.OK();
+        return;
+      }
+      this.output += input;
+    }
+    return;
+  }
+  clear() {
+    this.output = "0";
+  }
+  remove() {
+    if (this.output.length !== 1) this.output = this.output.slice(0, -1);
+  }
+  OK() {
+    console.log("OK");
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
-  .numberPad{
-    .output{
-      @extend %clearFix;
-      @extend %innerShadow;
-      font-size:36px;
-      font-family:Consolas,monospace;
-      padding: 9px 16px;
-      text-align:right;
-    }
-    .buttons{
-      @extend %clearFix;
-      > button{
-        width: 25%;
-        height: 64px;
-        float: left;
-        background:transparent;
-        border:none;
-        &.OK{
-          height: 64*2px;
-          float:right;
-        }
-        &.zero{
-          width: 25*2%;
-        }
-        $bg:#f2f2f2;
-        &:nth-child(2),&:nth-child(5){
-          background: darken($bg,4*2%);
-        }
-        &:nth-child(3), &:nth-child(6), &:nth-child(9) {
-          background: darken($bg, 4*2%);
-        }
-        &:nth-child(4), &:nth-child(7), &:nth-child(10) {
-          background: darken($bg, 4*3%);
-        }
-        &:nth-child(8), &:nth-child(11), &:nth-child(13) {
-          background: darken($bg, 4*4%);
-        }
-        &:nth-child(14) {
-          background: darken($bg, 4*5%);
-        }
-        &:nth-child(12) {
-          background: darken($bg, 4*6%);
-        }
+.numberPad {
+  .output {
+    @extend %clearFix;
+    @extend %innerShadow;
+    font-size: 36px;
+    font-family: Consolas, monospace;
+    padding: 9px 16px;
+    text-align: right;
+  }
+  .buttons {
+    @extend %clearFix;
+    > button {
+      width: 25%;
+      height: 64px;
+      float: left;
+      background: transparent;
+      border: none;
+      &.OK {
+        height: 64 * 2px;
+        float: right;
+      }
+      &.zero {
+        width: 25 * 2%;
+      }
+      $bg: #f2f2f2;
+      &:nth-child(2),
+      &:nth-child(5) {
+        background: darken($bg, 4 * 2%);
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background: darken($bg, 4 * 2%);
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background: darken($bg, 4 * 3%);
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background: darken($bg, 4 * 4%);
+      }
+      &:nth-child(14) {
+        background: darken($bg, 4 * 5%);
+      }
+      &:nth-child(12) {
+        background: darken($bg, 4 * 6%);
       }
     }
   }
+}
 </style>
--- src/components/money/Types.vue
+++ src/components/money/Types.vue
@@ -11,7 +11,7 @@
 import Vue from 'vue'
 import {Component} from 'vue-property-decorator'
 @Component
-export default class extends Vue {
+export default class Types extends Vue {
   type = '-'
   selectType(type: string){
     if (type !== '-' && type !== '+') {
