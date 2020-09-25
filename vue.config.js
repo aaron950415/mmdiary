befd9876049ed7d1966b00a80c50dd88ab6da138
Aaron <aaron950415@qq.com>
2020-09-25 13:43:21 +1000

    部署网站到mmdiary-websit

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
 module.exports = { 
-  publicPath:'/',
+  publicPath:process.env.NODE_ENV === 'production'
+  ? '/mmdiary-websit/'
+  : '/',
   lintOnSave: false,
    chainWebpack: config => {    
       const dir = path.resolve(__dirname,'src/assets/icons')
